933a7b35ddcf8be1efc3d203260e1a4fcb00dfd8
JatinKumarShandilya <sigmajks2001@gmail.com>
2024-02-25 15:13:06 +0530

    week 2

---
+++ week_2/11_contain_duplicate.cpp
@@ -0,0 +1,17 @@
+// leetcode q - 217
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(auto a:nums){
+            m[a]++;
+        }
+        for(auto a:m){
+            if(a.second > 1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
