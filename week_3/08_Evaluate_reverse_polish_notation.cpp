1955e422c8db1999648247be0e8681d2afdb2a7f
JatinKumarShandilya <sigmajks2001@gmail.com>
2024-02-28 13:25:32 +0530

    week 3

---
+++ week_3/08_Evaluate_reverse_polish_notation.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> s;
+
+        for(int i=0;i<tokens.size();i++){
+            if(tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "/" && tokens[i] != "*")
+                s.push(stoi(tokens[i]));
+            else{
+                int x = s.top(); s.pop();
+                int y = s.top(); s.pop();
+                if(tokens[i] == "+"){
+                    s.push(y+x);
+                }
+                else if(tokens[i] == "-"){
+                    s.push(y-x);
+                }
+                else if(tokens[i] == "/"){
+                    s.push(y/x);
+                }
+                else if(tokens[i] == "*"){
+                    s.push(y*x);
+                }
+            }
+        }
+        return s.top();
+    }
+};
